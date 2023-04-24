70100ce11dfb420e18e299817d5c3047fe6a1923
f0xtr07 <34517817+f0xtr07@users.noreply.github.com>
2023-04-24 13:39:14 +0100

    Create squash.py
    Boilerplate for starting off with services URL constant strings.

---
+++ squash.py
@@ -0,0 +1,13 @@
+import random, string
+import requests as request
+
+def gen_combinations(name, mode, length, requestType):
+  GOOGLE_SHORT = "https://goo.gl"
+  TRADING212_SHORT = "https://www.trading212.com/invite/"
+  BITLY_SHORT = "https://bit.ly/"
+  GFORMS_SHORT = "https://forms.gle/"
+  
+
+if __name__ = '__main__':
+  print("Squash the URL! ")
+  
